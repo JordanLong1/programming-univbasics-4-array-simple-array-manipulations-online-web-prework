a9ed12c00dfd4cf19b4d8919b9a2752bf6081bf0
Jordan Long <jordanleelong5@gmail.com>
2019-09-20 03:12:23 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,27 @@
+def using_push(array, string)
+  array.push(string)
+end
+def using_unshift(array, string)
+  array.unshift(string)
+end
+def using_pop(array)
+  array.pop
+end
+def pop_with_args(array)
+  array.pop(2)
+end
+def using_shift(array)
+  array.shift
+end
+def shift_with_args(array)
+  array.shift(2)
+end
+def using_concat(array, new_array)
+  array.concat(new_array)
+end
+def using_insert(array, item)
+  array.insert(4, item)
+end
+def using_uniq(array)
+  array.uniq()
+end
